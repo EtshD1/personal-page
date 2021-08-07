8bad85b815651e9726225f2bb3a45b7819ddf1c5
EtshD1 <H79Pain@Gmail.com>
2021-08-07 23:03:22 +0200

    Add Vision Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Hero from './Hero-Page';
 import './index.scss';
+import Vision from './Vision';
 
 export const App = () => {
   return (<main id="App">
     <Hero />
+    <Vision />
   </main>);
 }
 
---
+++ src/Vision/index.tsx
@@ -0,0 +1,22 @@
+const Vision = () => {
+  return (<>
+    <div id="vision" className="container">
+      <h1>Vision</h1>
+      <div className="content">
+        <p>In our age, data travels fast. Having <span className="emphasis">the right online presence can promote your business fast enough since everyone is online</span>.</p>
+        <p>With today's technologies, having <span className="emphasis">the right online presence is only a website away</span>.</p>
+        <p>As a <span className="highlight">UI/UX Designer</span>, I create designs for users to have a smooth and optimal experience, and as a <span className="highlight">Front-End Developer</span>, I build applications with modern web technologies.</p>
+      </div>
+      <div className="nextSection">
+        <svg width="72" height="23" viewBox="0 0 72 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0.999993 1L36 21L71 1" stroke="#FDE7E8" strokeWidth="2" strokeLinecap="round" />
+        </svg>
+      </div>
+    </div>
+    <div className="clipedBG">
+      <div></div>
+    </div>
+  </>);
+}
+
+export default Vision;
