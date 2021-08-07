c51341ee61a9eb10826d7324711a34ce866909ea
EtshD1 <H79Pain@Gmail.com>
2021-08-08 00:55:25 +0200

    Add Images and Icons

---
+++ src/images/index.ts
@@ -0,0 +1,19 @@
+import Angular from './Angular.svg';
+import Figma from './Figma.svg';
+import NodeJS from './NodeJs.png';
+import Python from './Python.svg';
+import React from './React.svg';
+import TypeScript from './TypeScript.svg';
+import XD from './AdobeXD.svg';
+
+const images = {
+  Angular: Angular,
+  Figma: Figma,
+  NodeJS: NodeJS,
+  Python: Python,
+  React: React,
+  TypeScript: TypeScript,
+  XD: XD
+}
+
+export default images;
