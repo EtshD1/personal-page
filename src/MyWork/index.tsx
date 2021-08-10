80a2092a675f4c240deea15be07b5a391435e0dd
EtshD1 <H79Pain@Gmail.com>
2021-08-11 01:30:19 +0200

    fix svg attribute issues

--- src/MyWork/index.tsx
+++ src/MyWork/index.tsx
@@ -30,7 +30,7 @@ const MyWork = () => {
       {certsIsOpen ? <Certificates close={closeCerts} /> : null}
       <div className="nextSection" onClick={() => goto('work-together')}>
         <svg width="72" height="23" viewBox="0 0 72 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0.999993 1L36 21L71 1" stroke="white" stroke-width="2" stroke-linecap="round" />
+          <path d="M0.999993 1L36 21L71 1" stroke="white" strokeWidth="2" strokeLinecap="round" />
         </svg>
       </div>
     </div>
--- src/SkillsTech/index.tsx
+++ src/SkillsTech/index.tsx
@@ -10,7 +10,7 @@ const SkillsTech = () => {
     </div>
     <div className="nextSection" onClick={() => goto('my-work')}>
       <svg width="72" height="23" viewBox="0 0 72 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M1 1L36 21L71 1" stroke="#2D0B0B" stroke-width="2" stroke-linecap="round" />
+        <path d="M1 1L36 21L71 1" stroke="#2D0B0B" strokeWidth="2" strokeLinecap="round" />
       </svg>
     </div>
   </div>)
