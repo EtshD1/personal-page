aeee887b2feb36d166655c9c049e2e3d0014faf1
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-08-11 13:39:12 +0200

    Notify user with a humble banner

--- src/WorkTogether/EmailCopy.tsx
+++ src/WorkTogether/EmailCopy.tsx
@@ -1,20 +1,36 @@
+import { useState } from "react";
 import images from "../images";
 
 const EmailCopy = () => {
-  const copyEvent = () => {
-    const text = document.createElement("textarea");
-    text.value = "mohamed.h.abouelenin@gmail.com";
-    if (window.navigator.clipboard) {
-      window.navigator.clipboard.writeText(text.value);
-      alert("Email copied to clipboard");
-    } else {
+  const [banner, setBanner] = useState(false);
+  const [bannerText, setBannerText] = useState("");
 
+  const copyEvent = () => {
+    if (!banner) {
+      const text = document.createElement("textarea");
+      text.value = "mohamed.h.abouelenin@gmail.com";
+      if (window.navigator.clipboard) {
+        window.navigator.clipboard.writeText(text.value);
+        setBannerText("Email copied to clipboard.");
+        setBanner(true);
+        setTimeout(() => setBanner(false), 7000);
+      } else {
+        setBannerText("");
+        setBanner(true);
+        setTimeout(() => setBanner(false), 7000);
+      }
+      text.remove();
+    } else {
+      console.log("Why spam :(");
     }
   }
 
-  return (<div data-email="mohamed.h.abouelenin@gmail.com" onClick={copyEvent}>
-    <img src={images.Email} alt="" />
-  </div>);
+  return (<>
+    <div data-email="mohamed.h.abouelenin@gmail.com" onClick={copyEvent}>
+      <img src={images.Email} alt="" />
+    </div>
+    {banner ? <div className="banner">{bannerText}<a href="mailto:mohamed.h.abouelenin@gmail.com">Open in mail</a></div> : null}
+  </>);
 }
 
 export default EmailCopy;
